2148db1a4caecb66e3a49dff2e741eb0baf1b5ad
akashsirra <akashsirra24@gmail.com>
2026-09-08 16:29:23 +0530

    fix: invalidate stale World Explorer PWA cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='world-explorer-v5';
-const CORE=['/','/index.html','/style.css?v=5','/app.js?v=5','/manifest.webmanifest'];
+const CACHE='world-explorer-v6';
+const CORE=['/','/index.html','/style.css?v=6','/app.js?v=6','/manifest.webmanifest','/vendor/leaflet.js?v=6','/vendor/leaflet.css?v=6'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
